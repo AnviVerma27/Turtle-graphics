c819817a0492462c63e72b88b838b760293f7030
AnviVerma27 <anviver2005@gmail.com>
2023-01-12 20:31:05 +0530

    initial comit

---
+++ Turtle Graphics Name.py
@@ -0,0 +1,493 @@
+# for spacing in between two letters
+def spacing():
+    x.up()
+    x.fd(10)
+    x.down()
+
+
+# for letter A
+def A():
+    x.left(60)
+    x.fd(50)
+    x.right(120)
+    x.fd(20)
+    x.right(120)
+    x.fd(20)
+    x.bk(20)
+    x.left(120)
+    x.fd(30)
+    x.left(60)
+    spacing()
+
+
+# for letter B
+def B():
+    x.left(90)
+    x.fd(43.3)
+    x.right(90)
+    x.fd(20)
+    x.circle(-10.8, 180)
+    x.fd(20)
+    x.right(180)
+    x.fd(20)
+    x.circle(-10.8, 180)
+    x.fd(20)
+    x.up()
+    x.bk(40)
+    x.down()
+    x.right(180)
+
+
+# for letter C
+def C():
+    x.up()
+    x.fd(30)
+    x.left(90)
+    x.fd(43.3)
+    x.down()
+    x.left(90)
+    x.fd(10)
+    x.circle(21.6, 180)
+    x.fd(10)
+    spacing()
+
+
+# for letter D
+def D():
+    x.left(90)
+    x.fd(43.3)
+    x.right(90)
+    x.fd(10)
+    x.circle(-21.6, 180)
+    x.fd(10)
+    x.right(180)
+    x.up()
+    x.fd(32)
+    x.down()
+    spacing()
+
+
+# for letter E
+def E():
+    x.left(90)
+    x.fd(43.3)
+    x.right(90)
+    x.fd(30)
+    x.bk(30)
+    x.right(90)
+    x.fd(21.6)
+    x.left(90)
+    x.fd(15)
+    x.bk(15)
+    x.right(90)
+    x.fd(21.6)
+    x.left(90)
+    x.fd(30)
+    spacing()
+
+
+# for letter F
+def F():
+    x.left(90)
+    x.fd(43.3)
+    x.right(90)
+    x.fd(30)
+    x.bk(30)
+    x.right(90)
+    x.fd(21.6)
+    x.left(90)
+    x.fd(15)
+    x.bk(15)
+    x.right(90)
+    x.fd(21.6)
+    x.left(90)
+    x.up()
+    x.fd(30)
+    x.down()
+    spacing()
+
+
+# for letter G
+def G():
+    x.up()
+    x.fd(21.6)
+    x.left(90)
+    x.fd(43.3)
+    x.down()
+    x.left(90)
+    x.circle(21.6, 180)
+    x.circle(10.8, 180)
+    x.right(180)
+    x.fd(13)
+    x.right(90)
+    x.fd(21.6)
+    x.left(90)
+    spacing()
+
+
+# for letter H
+def H():
+    x.left(90)
+    x.fd(43.3)
+    x.bk(21.6)
+    x.right(90)
+    x.fd(25)
+    x.left(90)
+    x.fd(21.6)
+    x.bk(43.3)
+    x.right(90)
+    spacing()
+
+
+# for letter I
+def I():
+    x.left(90)
+    x.fd(43.3)
+    x.right(180)
+    x.fd(43.3)
+    x.left(90)
+    spacing()
+
+
+# for letter J
+def J():
+    x.left(90)
+    x.up()
+    x.fd(43.3)
+    x.down()
+    x.right(90)
+    x.fd(40)
+    x.bk(20)
+    x.right(90)
+    x.fd(33.3)
+    x.circle(-10, 180)
+    x.right(180)
+    x.up()
+    x.fd(10)
+    x.left(90)
+    x.fd(40)
+    x.down()
+    spacing()
+
+
+# for letter K
+def K():
+    x.left(90)
+    x.fd(43.3)
+    x.bk(21.6)
+    x.right(45)
+    x.fd(30.6)
+    x.bk(30.6)
+    x.right(90)
+    x.fd(30.6)
+    x.left(45)
+    spacing()
+
+
+# for letter L
+def L():
+    x.left(90)
+    x.fd(43.3)
+    x.bk(43.3)
+    x.right(90)
+    x.fd(30)
+    spacing()
+
+
+# for letter M
+def M():
+    x.left(90)
+    x.fd(43.3)
+    x.right(135)
+    x.fd(30)
+    x.left(90)
+    x.fd(30)
+    x.right(135)
+    x.fd(43.3)
+    x.left(90)
+    spacing()
+
+
+# for letter N
+def N():
+    x.left(90)
+    x.fd(43.3)
+    x.right(135)
+    x.fd(61.2)
+    x.left(135)
+    x.fd(43.3)
+    x.right(90)
+    spacing()
+    x.up()
+    x.right(90)
+    x.fd(43.3)
+    x.down()
+    x.left(90)
+
+
+# for letter O
+def O():
+    x.up()
+    x.fd(21.6)
+    x.down()
+    x.circle(21.6)
+    x.up()
+    x.fd(21.6)
+    x.down()
+    spacing()
+
+
+# for letter P
+def P():
+    x.left(90)
+    x.fd(43.3)
+    x.right(90)
+    x.fd(10)
+    x.circle(-10.8, 180)
+    x.fd(10)
+    x.left(90)
+    x.fd(21.6)
+    x.left(90)
+    x.up()
+    x.fd(10.8)
+    x.down()
+    spacing()
+
+
+# for letter Q
+def Q():
+    x.up()
+    x.fd(21.6)
+    x.down()
+    x.circle(21.6)
+    x.up()
+    x.fd(20)
+    x.down()
+    x.left(135)
+    x.fd(20)
+    x.bk(20)
+    x.right(135)
+    spacing()
+
+
+# for letter R
+def R():
+    x.left(90)
+    x.fd(43.3)
+    x.right(90)
+    x.fd(10)
+    x.circle(-10.8, 180)
+    x.fd(10)
+    x.left(135)
+    x.fd(30.6)
+    x.left(45)
+    spacing()
+
+
+# for letter S
+def S():
+    x.fd(20)
+    x.circle(10.8, 180)
+    x.fd(10)
+    x.circle(-10.8, 180)
+    x.fd(20)
+    x.up()
+    x.fd(1)
+    x.right(90)
+    x.fd(43.3)
+    x.left(90)
+    spacing()
+
+
+# for letter T
+def T():
+    x.up()
+    x.fd(15)
+    x.down()
+    x.left(90)
+    x.fd(43.3)
+    x.left(90)
+    x.fd(15)
+    x.bk(30)
+    x.left(90)
+    x.up()
+    x.fd(43.3)
+    x.down()
+    x.left(90)
+    spacing()
+
+
+# for letter U
+def U():
+    x.up()
+    x.left(90)
+    x.fd(43.3)
+    x.down()
+    x.right(180)
+    x.fd(28.3)
+    x.circle(15, 180)
+    x.fd(28.3)
+    x.right(180)
+    x.up()
+    x.fd(43.3)
+    x.down()
+    x.left(90)
+    spacing()
+
+
+# for letter V
+def V():
+    x.up()
+    x.left(90)
+    x.fd(43.3)
+    x.right(90)
+    x.down()
+    x.right(60)
+    x.fd(50)
+    x.left(120)
+    x.fd(50)
+    x.right(60)
+    spacing()
+    x.up()
+    x.right(90)
+    x.fd(43.3)
+    x.down()
+    x.left(90)
+
+
+# for letter W
+def W():
+    x.left(90)
+    x.fd(43.3)
+    x.left(180)
+    x.fd(43.3)
+    x.left(135)
+    x.fd(30)
+    x.right(90)
+    x.fd(30)
+    x.left(135)
+    x.fd(43.3)
+    x.bk(43.3)
+    x.right(90)
+    spacing()
+
+
+# for letter X
+def X():
+    x.left(60)
+    x.fd(49.8)
+    x.bk(24.9)
+    x.left(60)
+    x.fd(24.9)
+    x.bk(49.8)
+    x.right(120)
+    spacing()
+
+
+# for letter Y
+def Y():
+    x.left(60)
+    x.fd(49.8)
+    x.bk(24.9)
+    x.left(60)
+    x.fd(24.9)
+    x.up()
+    x.bk(49.8)
+    x.right(120)
+    x.down()
+    spacing()
+
+
+# for letter Z
+def Z():
+    x.left(90)
+    x.up()
+    x.fd(43.3)
+    x.down()
+    x.right(90)
+    x.fd(43.3)
+    x.right(135)
+    x.fd(61.2)
+    x.left(135)
+    x.fd(43.3)
+    spacing()
+
+
+a = input("enter your name (in all caps)")
+
+# importing libraries
+import turtle
+import time
+
+x = turtle.Turtle()
+y = turtle.Screen()
+turtle.colormode(255)
+y.bgcolor(200, 50, 100)
+y.bgpic("sample.gif")
+
+# positioning of turtle at initial point
+x.up()
+x.goto(-200, -10)
+x.down()
+x.speed(5)
+
+b = len(a)
+for q in range(0, b):
+    if a[q] == "A":
+        A()
+    elif a[q] == "B":
+        B()
+    elif a[q] == "C":
+        C()
+    elif a[q] == "D":
+        D()
+    elif a[q] == "E":
+        E()
+    elif a[q] == "F":
+        F()
+    elif a[q] == "G":
+        G()
+    elif a[q] == "H":
+        H()
+    elif a[q] == "I":
+        I()
+    elif a[q] == "J":
+        J()
+    elif a[q] == "K":
+        K()
+    elif a[q] == "L":
+        L()
+    elif a[q] == "M":
+        M()
+    elif a[q] == "N":
+        N()
+    elif a[q] == "O":
+        O()
+    elif a[q] == "P":
+        P()
+    elif a[q] == "Q":
+        Q()
+    elif a[q] == "R":
+        R()
+    elif a[q] == "S":
+        S()
+    elif a[q] == "T":
+        T()
+    elif a[q] == "U":
+        U()
+    elif a[q] == "V":
+        V()
+    elif a[q] == "W":
+        W()
+    elif a[q] == "X":
+        X()
+    elif a[q] == "Y":
+        Y()
+    elif a[q] == "Z":
+        Z()
+    elif a[q] == " ":
+        for z in range(0, 4):
+            spacing()
+    else:
+        print("enter correct input")
+time.sleep(4)
